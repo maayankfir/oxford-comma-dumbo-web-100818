6acd1631a215ace0e0139527738ce556cef8da6b
Maayan Kfir <mayankfir@gmail.com>
2018-09-23 18:23:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    array[0]
+  elsif array.size == 2
+    "#{array[0]} and #{array[1]}"
+else
+  "#{array[0, array.length-1].join(", ")}, and #{array.last}"
+end
 end
