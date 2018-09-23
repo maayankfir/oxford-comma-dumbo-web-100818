ca8afd8ec19f7f384ea4c03525aa24e5b2455b4c
Maayan Kfir <mayankfir@gmail.com>
2018-09-23 18:20:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.size < 2
     array[0]
+  elsif array.size == 2
+    "#{array[0]} and #{array[1]}"
 else
   "#{array[0, array.length].join(", ")} and #{array.last}"
 end
